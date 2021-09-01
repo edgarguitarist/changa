eb22b38d25ac4cbabfa0ac9d961b71a6d343d967
edgarguitarist <edgarguitarist@gmail.com>
2021-09-01 13:52:53 -0500

    aaaaaaaaaa

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -8,10 +8,12 @@ $con = mysqli_connect("$host", "$user", "$pass", "$name");
 
 // Check connection
 if (mysqli_connect_errno()) {  
+  echo "<script>console.log(':C')</script>";
   mysqli_close($con);
   $bandera = true;
 }else{
   echo "<script>//console.log('Usted esta en el entorno Local')</script>";
+  mysqli_query($con, "SET NAMES 'utf8'");
 }
 
 if($bandera){
@@ -21,7 +23,8 @@ if($bandera){
   $name = 'u440807742_prueba';
   $con = mysqli_connect("$host", "$user", "$pass", "$name");
   echo "<script>//console.log('Usted esta en el entorno Online')</script>";
+  mysqli_query($con, "SET NAMES 'utf8'");
 }
 
-mysqli_query($con, "SET NAMES 'utf8'");
+
 ?>
