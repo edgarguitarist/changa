92f649f1588c88134967083d04ff3eaab5a95f7a
edgarguitarist <edgarguitarist@gmail.com>
2022-04-17 13:35:16 -0500

    another change

--- student/progress_menu.php
+++ student/progress_menu.php
@@ -26,23 +26,23 @@
 <div id="p_container">
     <div class='recuadro'>
         <div class="centrador">
-            <a class="is-size-4 button is-link is-outlined" href="progress_apk.php?id=<?= $get_id ?>&user_id=<?= $session_id ?>&view=practica"><em class="fas fa-book-reader"></em>Deberes</a>
+            <a class="is-size-3 button is-link is-outlined" href="progress_apk.php?id=<?= $get_id ?>&user_id=<?= $session_id ?>&view=practica"><em class="fas fa-book-reader"></em>Deberes</a>
         </div>
     </div>
     <div class='recuadro'>
         <div class="centrador">
-            <a class="is-size-4 button is-link is-outlined" href="progress_apk.php?id=<?= $get_id ?>&user_id=<?= $session_id ?>&view=exam"><em class="fas fa-book-open"></em> Examenes</a>
+            <a class="is-size-3 button is-link is-outlined" href="progress_apk.php?id=<?= $get_id ?>&user_id=<?= $session_id ?>&view=exam"><em class="fas fa-book-open"></em> Examenes</a>
         </div>
     </div>
     <br>
     <div class='recuadro'>
         <div class="centrador">
-            <a class="is-size-4 button is-link is-outlined" href="progress_apk.php?id=<?= $get_id ?>&user_id=<?= $session_id ?>&view=games"><em class="fas fa-puzzle-piece"></em> Juegos</a>
+            <a class="is-size-3 button is-link is-outlined" href="progress_apk.php?id=<?= $get_id ?>&user_id=<?= $session_id ?>&view=games"><em class="fas fa-puzzle-piece"></em> Juegos</a>
         </div>
     </div>
     <div class='recuadro'>
         <div class="centrador">
-            <a class="is-size-4 button is-link is-outlined" href="progress_apk.php?id=<?= $get_id ?>&user_id=<?= $session_id ?>&view=histories"><em class="fas fa-theater-masks"></em> Historias</a>
+            <a class="is-size-3 button is-link is-outlined" href="progress_apk.php?id=<?= $get_id ?>&user_id=<?= $session_id ?>&view=histories"><em class="fas fa-theater-masks"></em> Historias</a>
         </div>
     </div>
 </div>
